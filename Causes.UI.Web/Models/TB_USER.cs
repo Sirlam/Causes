dc213616fed3da370c8b89116462b9171b22e78e
Salam Olanrewaju <salam_lanre@yahoo.com>
2019-02-15 14:38:53 +0100

    Initial Commit: with login page

---
+++ Causes.UI.Web/Models/TB_USER.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Causes.UI.Web.Models
+{
+    public class TB_USER
+    {
+
+    }
+}
---
+++ Causes.UI.Web/ViewModels/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Causes.UI.Web.ViewModels
+{
+    public class User
+    {
+        [Required(ErrorMessage ="Email is required", AllowEmptyStrings =false)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required", AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
