d12043cca1406998465bf08a1f4566e8fd3be1ca
Salam Olanrewaju <salam_lanre@yahoo.com>
2019-02-22 11:34:43 +0100

    Fixing the landing page

--- Causes.UI.Web/Controllers/HomeController.cs
+++ Causes.UI.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Causes.UI.Web.DAC;
+using Causes.UI.Web.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,30 @@ namespace Causes.UI.Web.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        CausesDAC _causesDAC;
+
+        public HomeController()
+        {
+            _causesDAC = new CausesDAC();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Cause> model = new List<Cause>();
+            var items = _causesDAC.SelectAllCauses();
+
+            foreach(var item in items)
+            {
+                Cause cause = new Cause();
+                cause.ID = item.ID;
+                cause.IMG_URL = item.IMG_URL;
+                cause.TOPIC = item.TOPIC;
+                cause.DESCRIPTION = item.DESCRIPTION;
+                cause.CREATED_BY = item.CREATED_BY;
+                cause.CREATED_DATE = item.CREATED_DATE;
+                model.Add(cause);
+            }
+            return View(model);
         }
     }
 }
